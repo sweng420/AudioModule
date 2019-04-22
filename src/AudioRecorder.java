dd83dca779a48bc8e4db22c6de6853e1d5ce92c5
tlc511 <tlc511@york.ac.uk>
2019-04-22 22:16:41 +0100

    v5.0: Implemented Session ID functionality fully and pause-N-times functionality as well - cleanup and refactor required

--- src/AudioRecorder.java
+++ src/AudioRecorder.java
@@ -19,46 +19,55 @@
 
 // TODO: user defined file path
 // TODO: implement gain control: https://docs.oracle.com/javase/tutorial/sound/controls.html 
-// TODO: Generate 'session ID' to implement pausing across given time period?
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Scanner;
 
 import javax.sound.sampled.*;
-import javax.sound.sampled.AudioFileFormat.Type;
 
 public class AudioRecorder
 {
 	static TargetDataLine targetLine = null;
-	static final long maxRecordingTime = 3600000; // One hour in milliseconds
+	static final long maxRecordingTime = 3600000; 								// One hour in milliseconds
 	static Thread recordThread;
 	static boolean pauseFlag = false;
 	
 	static String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date()); // Today's date
-	static File dir = new File("C:/temp/"); // Root directory
-	static String fileNameWithSessionID; // Output filename 
-	static File outputFile = new File("C:/temp/" + date + ".wav"); 
-	static File tempFile = new File("C:/temp/temp.wav"); 
-	static AudioInputStream audioInputStream;
+	static File dir = new File("C:/temp/"); 									// Root directory
+	static String fileNameWithSessionID; 										// Output filename 
+	static File recordFile = new File("C:/temp/" + date + ".wav"); 				// First file to be recorded to
+	static File tempFile = new File("C:/temp/temp.wav"); 						// Second file to be appended to first file
+	static File copyFile;
+	static File appendedFile;													// Output file
+	static AudioInputStream audioInputStream; 									// Microphone input stream 
 	
 	static int sessionID = 1;
 	
 	public static void main(String[] args)
 	{
-		File[] matches = dir.listFiles(new FilenameFilter()
-		{
-		  public boolean accept(File dir, String name)
-		  {
-		     return name.startsWith(date) && name.endsWith(".wav");
-		  }
-		});
+		// Ensure that there is a unique session ID for this session 
 		
-		System.out.println(Arrays.toString(matches));
+		generateSessionID();
+		
+		// Create output file
+		
+		createNewOutputFile();
+				
+		// TEST
+		
+//		manuallyEditSessionID();
+//		
+//		System.out.println(sessionID);
+		
+		// /TEST
 		
 		try
 		{
@@ -68,54 +77,44 @@ public class AudioRecorder
 		}
 		catch (LineUnavailableException e)
 		{
+			System.out.println("");
 			e.printStackTrace();
 		}
 		
 		// TEST
 		
-		startRecording();
-		
-		try
+		for (int i = 0; i < 5; i++)
 		{
-			Thread.sleep(5000);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		
-		stopRecording();
-		
-		try
-		{
-			Thread.sleep(500);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		
-		startRecording();
-		
-		try
-		{
-			Thread.sleep(5000);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
+			startRecording();
+			
+			try
+			{
+				Thread.sleep(5000);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			
+			stopRecording();
+			
+			try
+			{
+				Thread.sleep(500);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
 		}
 		
-		stopRecording();
-		
-		try 
-		{
-			Files.delete(outputFile.toPath());
-			Files.delete(tempFile.toPath());
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+//		try {
+//			Files.copy(copyFile.toPath(), appendedFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+//			Files.delete(copyFile.toPath());
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	
 	private static void initialiseAudioSettings() throws LineUnavailableException
@@ -141,7 +140,7 @@ public class AudioRecorder
 			System.out.println("Microphone detected. Querying target data line...");
 		}
 				
-		// Use metadata to ascertain whether audio format is supported by default input device
+		// Use metadata variable to ascertain whether audio format is supported by default input device
 				
 		if (AudioSystem.isLineSupported(audioFormatInfo) == false)
 		{
@@ -162,7 +161,7 @@ public class AudioRecorder
 			targetLine.open();
 		} catch (LineUnavailableException e1)
 		{
-			// TODO Auto-generated catch block
+			System.out.println("No line available for audio capture");
 			e1.printStackTrace();
 		}
 		
@@ -170,17 +169,9 @@ public class AudioRecorder
 		
 		audioInputStream = new AudioInputStream(targetLine);
 		
-		TimerTask scheduleRecordingEnd = new TimerTask()
-			{
-				public void run()
-				{
-					stopRecording();
-				}
-			};
-				
-		Timer recordingTimer = new Timer("Recording Timer");
+		// Ensure recording cannot exceed one hour
 		
-		recordingTimer.schedule(scheduleRecordingEnd, maxRecordingTime);
+		scheduleRecordingEnd();
 		
 		// Prepare line for audio input
 		
@@ -197,18 +188,14 @@ public class AudioRecorder
 				{
 					// If output file already exists, we must append it
 
-					if (outputFile.exists() && !outputFile.isDirectory())
+					if (recordFile.exists() || appendedFile.length() != 0)
 					{
-						//System.out.println("TEST: File existed!");
-
 						AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, tempFile);
 					} else
 					{
-						//System.out.println("TEST: File did not yet exist!");
-
 						// If output file does not yet exist, write audio input stream to file
 
-						AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, outputFile);
+						AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, recordFile);
 					}
 				} catch (IOException e)
 				{
@@ -249,7 +236,15 @@ public class AudioRecorder
 		
 		if (pauseFlag == true)
 		{
-			appendWavFiles(outputFile, tempFile);
+			if (appendedFile.exists())
+			{
+				System.out.println("Appending appendedFile and tempFile");
+				appendWavFiles(appendedFile, tempFile, false);
+			} else
+			{
+				System.out.println("Appending recordFile and tempFile");
+				appendWavFiles(recordFile, tempFile, true);
+			}
 		}
 		
 		System.out.println("Recording stopped...");
@@ -259,13 +254,10 @@ public class AudioRecorder
 	
 	// Append two .wav files and store them in a separate file
 	
-	private static void appendWavFiles(File fileOne, File fileTwo)
+	private static void appendWavFiles(File fileOne, File fileTwo, boolean deleteFileOneFlag)
 	{
 		try
-		{
-			fileNameWithSessionID = date.concat("_Session" + sessionID + ".wav");
-			File appendedFile = new File("C:/temp/" + fileNameWithSessionID);
-			
+		{	
 			AudioInputStream streamOne = AudioSystem.getAudioInputStream(fileOne);
 			AudioInputStream streamTwo = AudioSystem.getAudioInputStream(fileTwo);
 			
@@ -274,17 +266,146 @@ public class AudioRecorder
 													streamOne.getFormat(),
 													streamOne.getFrameLength() + streamTwo.getFrameLength());
 			
-			AudioSystem.write(appendedStreams, AudioFileFormat.Type.WAVE, appendedFile);
+			String fileNameWithSessionIDCopy = date.concat("_Session" + sessionID);
+			copyFile = new File("C:/temp/" + fileNameWithSessionIDCopy + "_Copy.wav");
 			
+			AudioSystem.write(appendedStreams, AudioFileFormat.Type.WAVE, appendedFile);
+			AudioSystem.write(appendedStreams, AudioFileFormat.Type.WAVE, copyFile);
+		
 			streamOne.close();
 			streamTwo.close();
 			appendedStreams.close();
 			
 			System.gc(); // lol
+			
+			Files.copy(copyFile.toPath(), appendedFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.delete(copyFile.toPath());
+			
+			try 
+			{
+				if (deleteFileOneFlag == true)
+					{
+						Files.delete(fileOne.toPath());
+					}
+				
+				Files.delete(fileTwo.toPath());
+				
+			} catch (IOException e)
+			{
+				//e.printStackTrace();
+			}
 		} catch (Exception e)
 		{
 			System.out.println("An error occurred while appending files");
 			e.printStackTrace();
 		}
+	}
+	
+	// Function to generate unique session ID every time the system is run 
+	
+	private static void generateSessionID()
+	{
+		// Query root directory for all files time-stamped with todays date
+		// Store the result in an array of file references
+		
+		File[] matches = dir.listFiles(new FilenameFilter()
+		{
+		  public boolean accept(File dir, String name)
+		  {
+			  // Since we are not interested in sessions from previous dates
+			  // We can safely ignore these and only search for today's sessions
+			  // So we return all WAVE files stamped with todays date 
+					  
+;		     return name.startsWith(date) && name.endsWith(".wav");
+		  }
+		});
+		
+		// Instantiate ArrayList to store information on Session IDs currently in use for this date
+
+		ArrayList<Integer> sessionIDsInUse = new ArrayList<Integer>();
+		
+		// Extract session IDs from matching audio files
+		
+		for (int i = 0; i < matches.length; i++)
+		{
+			// Extract filepath from matches
+			
+			String name = matches[i].toString();
+			
+			// Extract session ID from filepath and convert it to integer
+			
+			int extractedSessionID;
+			
+			try
+			{
+				extractedSessionID = Integer.parseInt(name.substring(26, name.length() - 4));
+				
+				// Add it to list of session IDs already being used
+				
+				sessionIDsInUse.add(extractedSessionID);
+			
+			} catch (NumberFormatException e)
+			{
+				// Do nothing...
+			}
+		}
+		
+		// If there are already files from today, find next available
+		// session ID, else do nothing
+		
+		if (sessionIDsInUse.isEmpty() == false)
+		{
+			// Find maximum session ID in use
+			
+			int maxIDInUse = Collections.max(sessionIDsInUse);
+			
+			// Set the session number equal to the next highest available session ID
+			
+			sessionID = maxIDInUse + 1;
+		}
+	}
+	
+	// Function to select session ID number manually
+	
+	private static void manuallyEditSessionID()
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter new Session ID : ");
+		String s = input.next();
+		
+		try
+		{
+			sessionID = Integer.parseInt(s);
+		} catch (NumberFormatException e)
+		{
+			System.out.println("Illegal session ID format - please enter a number");
+			e.printStackTrace();
+		} finally
+		{
+			input.close();
+		}
+	}
+	
+	private static void scheduleRecordingEnd()
+	{
+		TimerTask scheduleRecordingEnd = new TimerTask()
+		{
+			public void run()
+			{
+				stopRecording();
+			}
+		};
+			
+		Timer recordingTimer = new Timer("Recording Timer");
+	
+		recordingTimer.schedule(scheduleRecordingEnd, maxRecordingTime);
+	}
+	
+	private static void createNewOutputFile()
+	{	
+		fileNameWithSessionID = date.concat("_Session" + sessionID + ".wav");
+		appendedFile = new File("C:/temp/" + fileNameWithSessionID);
+		System.out.println("Created output file with name: " + fileNameWithSessionID);
 	}
 }
